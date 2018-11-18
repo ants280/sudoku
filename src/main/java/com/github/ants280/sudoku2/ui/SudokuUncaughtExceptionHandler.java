43aecbf501405ba95d34900029eb53aa5074b9ea
Jacob Patterson <jacob.patterson@gmail.com>
2018-11-18 11:22:26 -0800

    removing try/tach from SudokuUncaughtExceptionHandler, cleaning up file

--- src/main/java/com/github/ants280/sudoku2/ui/SudokuUncaughtExceptionHandler.java
+++ src/main/java/com/github/ants280/sudoku2/ui/SudokuUncaughtExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.github.ants280.sudoku2.ui;
 
 import java.awt.Component;
-import java.awt.HeadlessException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.Thread.UncaughtExceptionHandler;
@@ -28,36 +27,30 @@ public class SudokuUncaughtExceptionHandler implements UncaughtExceptionHandler
 	@Override
 	public void uncaughtException(Thread t, Throwable e)
 	{
-		try
-		{
-			JTextArea textArea = new JTextArea(getFormattedStackTrace(e), 15, 30);
+		JTextArea textArea = new JTextArea(getFormattedStackTrace(e), 15, 30);
 
-			JMenuItem copyMenuItem = new JMenuItem(new DefaultEditorKit.CopyAction());
-			JPopupMenu popupMenu = new JPopupMenu();
-			popupMenu.add(copyMenuItem);
-			textArea.addCaretListener(caretEvent -> copyMenuItem.setEnabled(caretEvent.getDot() != caretEvent.getMark()));
-			textArea.setComponentPopupMenu(popupMenu);
+		JMenuItem copyMenuItem = new JMenuItem(
+				new DefaultEditorKit.CopyAction());
+		JPopupMenu popupMenu = new JPopupMenu();
+		popupMenu.add(copyMenuItem);
+		textArea.addCaretListener(caretEvent -> copyMenuItem.setEnabled(
+				caretEvent.getDot() != caretEvent.getMark()));
+		textArea.setComponentPopupMenu(popupMenu);
 
-			JPanel panel = new JPanel();
-			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-			panel.add(new JLabel(e.getMessage()));
-			panel.add(new JScrollPane(textArea));
-			panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.add(new JLabel(e.getMessage()));
+		panel.add(new JScrollPane(textArea));
+		panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-			JOptionPane.showOptionDialog(
-					parentComponent,
-					panel,
-					"Error",
-					JOptionPane.DEFAULT_OPTION,
-					JOptionPane.ERROR_MESSAGE,
-					null,
-					null,
-					null);
-		}
-		catch (HeadlessException throwable)
-		{
-			throw new RuntimeException(throwable);
-		}
+		JOptionPane.showOptionDialog(
+				parentComponent,
+				panel,
+				"Error",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE,
+				null,
+				null,
+				null);
 	}
 
 	private static String getFormattedStackTrace(Throwable e)
