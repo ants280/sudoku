c666cf74cfe4a4317dd53923c2853afcb3380be7
Jacob Patterson <jacob.patterson@gmail.com>
2018-11-28 11:32:23 -0800

    moving all problem boards to SudokuBruteForceSolverTest.  This slows tests, but provides incentive to improve the logic solver!

--- src/main/java/com/github/ants280/sudoku/game/SudokuBoard.java
+++ src/main/java/com/github/ants280/sudoku/game/SudokuBoard.java
@@ -7,6 +7,7 @@ import java.util.Collections;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -18,7 +19,7 @@ public class SudokuBoard
 	private static final Predicate<List<SudokuCell>> CELL_SECTION_HAS_ALL_VALUES
 			= sudokuCells -> sudokuCells.stream()
 					.map(SudokuCell::getValue)
-					.filter(v -> v != null)
+					.filter(Objects::nonNull)
 					.mapToInt(Integer::intValue)
 					.map(i -> 1 << i)
 					.sum() == ((1 << 10) - 2);
@@ -40,14 +41,6 @@ public class SudokuBoard
 	public SudokuBoard()
 	{
 		this("{000000000000000000000000000000000000000000000000000000000000000000000000000000000}");
-		// problem boards:
-//		this("{079010004000560080000070906104080090000000000050030701701040000040098000900050430}"); // 5/5 stars March 22
-//		this("{370095000600080090008300007000010050160000034040060000700002900020030008000950042}"); // 6/5 stars 20170218
-//		this("{679810004410569087000070916104080090097020000050930701701040009040098170900050430}");
-//		this("{605040902001090003020015600900004800000060000008100009006430080500080200807020306}"); // 4/5 stars 2017-11-16
-//		this("{000062507006000001010070008095400002600090005100005970700040050200000800401580000}"); // 5/5 stars 2017-11-17
-//		this("{004063100000010002000074683907000000006080900000000504825640000700090000009350700}"); // 6/5 stars 2017-11-18
-//		this("{003070600000159020900000005700000010006040900040000006400000002070362000009080700}"); // :(
 	}
 
 	@Override
