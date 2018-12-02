8abc78b364cbd727e507239c5c07189d5a91ea75
Jacob Patterson <jacob.patterson@gmail.com>
2018-12-02 11:20:40 -0800

    implementing code for RemovePossibleValueForOtherGroupsSudokuSolverPlugin

--- src/main/java/com/github/ants280/sudoku/game/solver/RemovePossibleValueForOtherGroupsSudokuSolverPlugin.java
+++ src/main/java/com/github/ants280/sudoku/game/solver/RemovePossibleValueForOtherGroupsSudokuSolverPlugin.java
@@ -1,6 +1,12 @@
 package com.github.ants280.sudoku.game.solver;
 
+import com.github.ants280.sudoku.game.SectionType;
 import com.github.ants280.sudoku.game.SudokuBoard;
+import com.github.ants280.sudoku.game.SudokuCell;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RemovePossibleValueForOtherGroupsSudokuSolverPlugin
 		extends SudokuSolverPlugin
@@ -14,6 +20,76 @@ public class RemovePossibleValueForOtherGroupsSudokuSolverPlugin
 	@Override
 	public boolean makeMove()
 	{
+		for (int groupIndex = 0; groupIndex < 9; groupIndex++)
+		{
+			List<SudokuCell> groupCells
+					= sudokuBoard.getSudokuCells(
+							SectionType.GROUP,
+							groupIndex);
+
+			if (this.tryToRemovePossibleValuesForOtherGroups(groupCells, groupIndex, SectionType.COLUMN)
+					|| this.tryToRemovePossibleValuesForOtherGroups(groupCells, groupIndex, SectionType.ROW))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private boolean tryToRemovePossibleValuesForOtherGroups(
+			List<SudokuCell> groupCells,
+			int groupIndex,
+			SectionType sectionType)
+	{
+		List<SudokuCell> valuelessGroupCells
+				= groupCells.stream()
+						.filter(sudokuCell -> sudokuCell.getValue() == null)
+						.collect(Collectors.toList());
+		Map<Integer, List<SudokuCell>> cellsInGroupBySectionType
+				= valuelessGroupCells.stream()
+						.collect(Collectors.groupingBy(
+								sudokuCell -> sudokuCell.getIndex(
+										sectionType)));
+
+		for (Map.Entry<Integer, List<SudokuCell>> sectionTypeInGroupEntry
+				: cellsInGroupBySectionType.entrySet())
+		{
+			Integer sectionTypeIndex = sectionTypeInGroupEntry.getKey();
+			List<Integer> possibleValuesInSectionTypeIndex
+					= sectionTypeInGroupEntry.getValue()
+							.stream()
+							.map(SudokuCell::getPossibleValues)
+							.flatMap(Collection::stream)
+							.distinct()
+							.collect(Collectors.toList());
+			for (Integer possibleValue : possibleValuesInSectionTypeIndex)
+			{
+				boolean possibleValueOnlyInSectionTypeIndex = valuelessGroupCells.stream()
+						.filter(groupCell -> groupCell.getIndex(sectionType) != sectionTypeIndex)
+						.allMatch(groupCell -> !groupCell.getPossibleValues().contains(possibleValue));
+
+				if (possibleValueOnlyInSectionTypeIndex)
+				{
+					List<SudokuCell> otherCellsInSectionType
+							= sudokuBoard.getSudokuCells(sectionType, sectionTypeIndex)
+									.stream()
+									.filter(sudokuCell -> sudokuCell.getValue() == null
+									&& sudokuCell.getIndex(SectionType.GROUP) != groupIndex
+									&& sudokuCell.hasPossibleValue(possibleValue))
+									.collect(Collectors.toList());
+
+					if (!otherCellsInSectionType.isEmpty())
+					{
+						otherCellsInSectionType
+								.forEach(sudokuCell -> sudokuCell.togglePossibleValue(possibleValue));
+
+						return true;
+					}
+				}
+			}
+		}
+
 		return false;
 	}
 }
