9e0da09f29feac3dcbc1e16d63617cb6719cd2a2
Jacob Patterson <jacob.patterson@gmail.com>
2018-12-29 19:56:52 -0800

    adding test to ensure resetting a board clears the command history

--- src/test/java/com/github/ants280/sudoku/game/undo/SudokuUndoBoardTest.java
+++ src/test/java/com/github/ants280/sudoku/game/undo/SudokuUndoBoardTest.java
@@ -1,5 +1,8 @@
 package com.github.ants280.sudoku.game.undo;
 
+import com.github.ants280.sudoku.game.SudokuBoard;
+import com.github.ants280.sudoku.game.SudokuValue;
+import java.util.function.BiConsumer;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -26,5 +29,23 @@ public class SudokuUndoBoardTest
 				.forEach(sudokuCell -> Assert.assertSame(
 				commandHistory,
 				((SudokuUndoCell) sudokuCell).getCommandHistory()));
+	}
+
+	@Test
+	public void testResetFrom_undoHistoryCleared()
+	{
+		BiConsumer<Boolean, Boolean> undoRedoEmptyConsumer = (undoEmpty, redoEmpty) ->
+		{
+		};
+		CommandHistory<SudokuUndoCellCommand> commandHistory
+				= new CommandHistory<>(undoRedoEmptyConsumer);
+		SudokuUndoBoard sudokuUndoBoard = new SudokuUndoBoard(commandHistory);
+
+		sudokuUndoBoard.getAllSudokuCells()
+				.get(0)
+				.setValue(SudokuValue.VALUE_1);
+		sudokuUndoBoard.resetFrom(new SudokuBoard());
+
+		Assert.assertEquals(0, commandHistory.getUndoCount());
 	}
 }
