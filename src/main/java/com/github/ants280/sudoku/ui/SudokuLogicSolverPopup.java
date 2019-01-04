6b7902b1859cf27270f108cad29169de36695aa7
Jacob Patterson <jacob.patterson@gmail.com>
2019-01-04 10:56:24 -0800

    adding todo

--- src/main/java/com/github/ants280/sudoku/ui/SudokuLogicSolverPopup.java
+++ src/main/java/com/github/ants280/sudoku/ui/SudokuLogicSolverPopup.java
@@ -282,6 +282,7 @@ public class SudokuLogicSolverPopup implements ActionListener, ChangeListener
 		@Override
 		public void windowClosing(WindowEvent e)
 		{
+			// TODO: Remove listeners
 			timer.stop();
 		}
 	}
