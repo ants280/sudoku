f1de9c62f977c367de20af9a9e2cd12a5aea6a94
Jacob Patterson <jacob.patterson@gmail.com>
2019-01-15 13:02:33 -0800

    moving all painting code into inner class SudokuDisplayComponentImpl.

--- src/main/java/com/github/ants280/sudoku/ui/SudokuDisplayComponent.java
+++ src/main/java/com/github/ants280/sudoku/ui/SudokuDisplayComponent.java
@@ -91,168 +91,163 @@ public class SudokuDisplayComponent
 		component.repaint();
 	}
 
-	//<editor-fold defaultstate="collapsed" desc="painting">
 	private class SudokuDisplayComponentImpl extends JComponent
 	{
 		private static final long serialVersionUID = 1L;
 
+		//<editor-fold defaultstate="collapsed" desc="painting">
 		@Override
 		protected void paintComponent(Graphics graphics)
 		{
 			((Graphics2D) graphics).setRenderingHints(ANTIALIAS_ON_RENDERING_HINT);
 
-			SudokuDisplayComponent.this.paintComponent(graphics);
+			this.paintSelectedCellBackground(graphics);
+			board.getAllSudokuCells()
+					.forEach(sudokuCell -> this.paintCell(sudokuCell, graphics));
+			this.paintLines(graphics);
 		}
-	}
 
-	private void paintComponent(Graphics graphics)
-	{
-		this.paintSelectedCellBackground(graphics);
-		board.getAllSudokuCells()
-				.forEach(sudokuCell -> this.paintCell(sudokuCell, graphics));
-		this.paintLines(graphics);
-	}
-
-	private void paintSelectedCellBackground(Graphics graphics)
-	{
-		if (selectedRow != null && selectedCol != null)
+		private void paintSelectedCellBackground(Graphics graphics)
 		{
-			graphics.setColor(Color.LIGHT_GRAY);
-			graphics.fillRect(
-					xOffset + (selectedCol * cellLength),
-					yOffset + (selectedRow * cellLength),
-					cellLength,
-					cellLength);
+			if (selectedRow != null && selectedCol != null)
+			{
+				graphics.setColor(Color.LIGHT_GRAY);
+				graphics.fillRect(
+						xOffset + (selectedCol * cellLength),
+						yOffset + (selectedRow * cellLength),
+						cellLength,
+						cellLength);
+			}
 		}
-	}
 
-	private void paintCell(SudokuCell sudokuCell, Graphics graphics)
-	{
-		if (sudokuCell.getValue() != null)
+		private void paintCell(SudokuCell sudokuCell, Graphics graphics)
 		{
-			graphics.setColor(sudokuCell.isLocked()
-					? Color.DARK_GRAY : Color.BLACK);
-			graphics.setFont(valueFont);
-			int charWidth = graphics.getFontMetrics()
-					.stringWidth(sudokuCell.getValue().getDisplayValue());
-			int fontHeightPx = (int) (valueFont.getSize() * 0.75d);
-			int cellColumn = sudokuCell.getIndex(SectionType.COLUMN);
-			int cellRow = sudokuCell.getIndex(SectionType.ROW);
-			int colOffset = (int) (xOffset - (charWidth / 2d)
-					+ (cellLength * (cellColumn + 0.5d)));
-			int rowOffset = (int) (yOffset + (fontHeightPx / 2d)
-					+ (cellLength * (cellRow + 0.5d)));
+			if (sudokuCell.getValue() != null)
+			{
+				graphics.setColor(sudokuCell.isLocked()
+						? Color.DARK_GRAY : Color.BLACK);
+				graphics.setFont(valueFont);
+				int charWidth = graphics.getFontMetrics()
+						.stringWidth(sudokuCell.getValue().getDisplayValue());
+				int fontHeightPx = (int) (valueFont.getSize() * 0.75d);
+				int cellColumn = sudokuCell.getIndex(SectionType.COLUMN);
+				int cellRow = sudokuCell.getIndex(SectionType.ROW);
+				int colOffset = (int) (xOffset - (charWidth / 2d)
+						+ (cellLength * (cellColumn + 0.5d)));
+				int rowOffset = (int) (yOffset + (fontHeightPx / 2d)
+						+ (cellLength * (cellRow + 0.5d)));
+
+				graphics.drawString(
+						sudokuCell.getValue().getDisplayValue(),
+						colOffset,
+						rowOffset);
+			}
+			else
+			{
+				if (!sudokuCell.getPossibleValues().isEmpty())
+				{
+					graphics.setColor(Color.BLACK);
+					graphics.setFont(possibleValueFont);
+					int fontHeightPx = (int) (possibleValueFont.getSize() * 0.75d);
+					FontMetrics fontMetrics = graphics.getFontMetrics();
+
+					sudokuCell.getPossibleValues()
+							.forEach(possibleValue -> this.paintPossibleCellValue(
+							sudokuCell,
+							possibleValue,
+							graphics,
+							fontMetrics,
+							fontHeightPx));
+				}
+			}
+		}
+
+		private void paintPossibleCellValue(
+				SudokuCell sudokuCell,
+				SudokuValue possibleValue,
+				Graphics graphics,
+				FontMetrics fontMetrics,
+				int fontHeightPx)
+		{
+			int charWidth = fontMetrics.stringWidth(possibleValue.getDisplayValue());
+			int possibleValueCol = (possibleValue.getValue() - 1) % 3;
+			int possibleValueRow = (possibleValue.getValue() - 1) / 3;
+			double colPercentage = sudokuCell.getIndex(SectionType.COLUMN)
+					+ ((1 + (2 * possibleValueCol)) / 6d);
+			double rowPercentage = sudokuCell.getIndex(SectionType.ROW)
+					+ ((1 + (2 * possibleValueRow)) / 6d);
+			int colOffset = (int) (xOffset + cellLength * colPercentage
+					- (charWidth / 2d));
+			int rowOffset = (int) (yOffset + cellLength * rowPercentage
+					+ (fontHeightPx / 2d));
 
 			graphics.drawString(
-					sudokuCell.getValue().getDisplayValue(),
-					colOffset,
-					rowOffset);
+					possibleValue.getDisplayValue(), colOffset, rowOffset);
 		}
-		else
+
+		private void paintLines(Graphics graphics)
 		{
-			if (!sudokuCell.getPossibleValues().isEmpty())
+			int boardLength = cellLength * 9;
+
+			graphics.setColor(Color.BLACK);
+			for (int i = 0; i <= 9; i++)
 			{
-				graphics.setColor(Color.BLACK);
-				graphics.setFont(possibleValueFont);
-				int fontHeightPx = (int) (possibleValueFont.getSize() * 0.75d);
-				FontMetrics fontMetrics = graphics.getFontMetrics();
+				int offset = i * cellLength;
 
-				sudokuCell.getPossibleValues()
-						.forEach(possibleValue -> this.paintPossibleCellValue(
-						sudokuCell,
-						possibleValue,
-						graphics,
-						fontMetrics,
-						fontHeightPx));
-			}
-		}
-	}
+				// row
+				graphics.drawLine(
+						xOffset + 0,
+						yOffset + offset,
+						xOffset + boardLength,
+						yOffset + offset);
 
-	private void paintPossibleCellValue(
-			SudokuCell sudokuCell,
-			SudokuValue possibleValue,
-			Graphics graphics,
-			FontMetrics fontMetrics,
-			int fontHeightPx)
-	{
-		int charWidth = fontMetrics.stringWidth(possibleValue.getDisplayValue());
-		int possibleValueCol = (possibleValue.getValue() - 1) % 3;
-		int possibleValueRow = (possibleValue.getValue() - 1) / 3;
-		double colPercentage = sudokuCell.getIndex(SectionType.COLUMN)
-				+ ((1 + (2 * possibleValueCol)) / 6d);
-		double rowPercentage = sudokuCell.getIndex(SectionType.ROW)
-				+ ((1 + (2 * possibleValueRow)) / 6d);
-		int colOffset = (int) (xOffset + cellLength * colPercentage
-				- (charWidth / 2d));
-		int rowOffset = (int) (yOffset + cellLength * rowPercentage
-				+ (fontHeightPx / 2d));
+				// col
+				graphics.drawLine(
+						xOffset + offset,
+						yOffset + 0,
+						xOffset + offset,
+						yOffset + boardLength);
 
-		graphics.drawString(
-				possibleValue.getDisplayValue(), colOffset, rowOffset);
-	}
-
-	private void paintLines(Graphics graphics)
-	{
-		int boardLength = cellLength * 9;
-
-		graphics.setColor(Color.BLACK);
-		for (int i = 0; i <= 9; i++)
-		{
-			int offset = i * cellLength;
-
-			// row
-			graphics.drawLine(
-					xOffset + 0,
-					yOffset + offset,
-					xOffset + boardLength,
-					yOffset + offset);
-
-			// col
-			graphics.drawLine(
-					xOffset + offset,
-					yOffset + 0,
-					xOffset + offset,
-					yOffset + boardLength);
-
-			// Paint thicker lines every three cells (except edges).
-			if (i % 3 == 0)
-			{
-				if (i != 0)
+				// Paint thicker lines every three cells (except edges).
+				if (i % 3 == 0)
 				{
-					// row
-					graphics.drawLine(
-							xOffset + 0,
-							yOffset + offset - 1,
-							xOffset + boardLength,
-							yOffset + offset - 1);
+					if (i != 0)
+					{
+						// row
+						graphics.drawLine(
+								xOffset + 0,
+								yOffset + offset - 1,
+								xOffset + boardLength,
+								yOffset + offset - 1);
 
-					// col
-					graphics.drawLine(
-							xOffset + offset - 1,
-							yOffset + 0,
-							xOffset + offset - 1,
-							yOffset + boardLength);
-				}
-				if (i != 9)
-				{
-					// row
-					graphics.drawLine(
-							xOffset + 0,
-							yOffset + offset + 1,
-							xOffset + boardLength,
-							yOffset + offset + 1);
+						// col
+						graphics.drawLine(
+								xOffset + offset - 1,
+								yOffset + 0,
+								xOffset + offset - 1,
+								yOffset + boardLength);
+					}
+					if (i != 9)
+					{
+						// row
+						graphics.drawLine(
+								xOffset + 0,
+								yOffset + offset + 1,
+								xOffset + boardLength,
+								yOffset + offset + 1);
 
-					// col
-					graphics.drawLine(
-							xOffset + offset + 1,
-							yOffset + 0,
-							xOffset + offset + 1,
-							yOffset + boardLength);
+						// col
+						graphics.drawLine(
+								xOffset + offset + 1,
+								yOffset + 0,
+								xOffset + offset + 1,
+								yOffset + boardLength);
+					}
 				}
 			}
 		}
+		//</editor-fold>
 	}
-//</editor-fold>
 
 	//<editor-fold defaultstate="collapsed" desc="selectedCell">
 	public void selectCellFromCoordinates(int x, int y)
